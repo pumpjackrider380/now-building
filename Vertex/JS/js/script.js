4a73232a18bb99fba9e44417f0d5a1d28ff4d620
LT <mthienle.vn@gmail.com>
2020-06-13 16:30:52 -0500

    upload

---
+++ Vertex/JS/js/script.js
@@ -0,0 +1,9 @@
+let drink = 'tea';
+
+if (drink = 'tea') {
+	console.log('I will have tea');
+} else if (drink = 'coffee') {
+	console.log('I will have coffee');
+} else {
+	console.log('stay at home');
+}
